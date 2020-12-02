0d1fd421faef2198cbfcbac40e9576fa1fb2692b
DanielGrJ2810 <73206195+DanielGrJ2810@users.noreply.github.com>
2020-12-01 21:52:56 -0600

    Maes aqui ya avance un poco el proyecto, si tienen alguna duda me dicen

--- src/proyecto_taller_fide/Proyecto_Taller_Fide.java
+++ src/proyecto_taller_fide/Proyecto_Taller_Fide.java
@@ -1,26 +1,16 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
-<<<<<<< HEAD
- * and open the template in the editor.
-duant
-=======
- Bryan en Prueba taller
->>>>>>> f657862ccbffc2fd2957ae9a9365dfe3c65ce068
+
  */
 package proyecto_taller_fide;
 
-/**
- *
- * @author Daniel
- */
+import javax.swing.*;
+
 public class Proyecto_Taller_Fide {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    public static void main(String[] args)
+    {
+       
     }
     
 }
