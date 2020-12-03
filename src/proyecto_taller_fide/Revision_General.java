5b28e25d4c4d62549b88d5968bf96aba52398f11
DanielGrJ2810 <73206195+DanielGrJ2810@users.noreply.github.com>
2020-12-03 12:33:58 -0600

    Aqui cree una variable cadena que nos puede servir para facturar todo lo que hagamos en la factura

--- src/proyecto_taller_fide/Revision_General.java
+++ src/proyecto_taller_fide/Revision_General.java
@@ -3,15 +3,24 @@
  */
 package proyecto_taller_fide;
 
+import javax.swing.*;
+
 
 public class Revision_General 
 {
  
+ 
+ 
+ 
+ 
     public void Revisar()
     {
+        
         Menú revisar= new Menú();
+        
         revisar.getContador();
         revisar.getA_pagar();
+       revisar.getCadena();
       
         Servicio datos[]=new Servicio [1];
        
@@ -26,10 +35,16 @@ public class Revision_General
            for(int j=revisar.getContador();j<revisar.getContador()+1;j++)
            {
                String nombre="Revision General";
-               int Gastos=(int)(revisar.getA_pagar()+revisar2.getRevisionGeneral());
+                int Gastos=(int)(revisar.getA_pagar()+revisar2.getRevisionGeneral());
                
                datos[j]=new Servicio(nombre, Gastos);
-           }
+               
+            revisar.setCadena((revisar.getCadena()+"\n"+"servicio: "+datos[j].getNombre()+"\n "+"Gastó: "+datos[j].getGastos()+" "));
+         
+        }
+           
+         
+       JOptionPane.showMessageDialog(null,revisar.getCadena());
            
            
        
@@ -39,4 +54,8 @@ public class Revision_General
        
        
     }
+    
+    
+    
+    
 }
