7395fbd84309aad1daee2a863ccbdc0b4a0dd45d
DanielGrJ2810 <73206195+DanielGrJ2810@users.noreply.github.com>
2020-12-15 16:26:24 -0600

    nuevo

--- src/proyecto_taller_fide/Proyecto_Taller_Fide.java
+++ src/proyecto_taller_fide/Proyecto_Taller_Fide.java
@@ -1,13 +1,13 @@
 /*
 
  */
-package proyecto_taller_fide;
+package proyecto_taller_fide; 
 
 import javax.swing.*;
 
 public class Proyecto_Taller_Fide {
 
-    
+
     public static void main(String[] args)
     {
         Menú proyecto=new Menú();
