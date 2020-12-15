a79613a135dd0aae7df30b031b3cb2cbb6f42bb9
DanielGrJ2810 <73206195+DanielGrJ2810@users.noreply.github.com>
2020-12-15 16:30:50 -0600

    nuevo Revision_General!!

--- src/proyecto_taller_fide/Revision_General.java
+++ src/proyecto_taller_fide/Revision_General.java
@@ -3,59 +3,25 @@
  */
 package proyecto_taller_fide;
 
-import javax.swing.*;
+import javax.swing.*; 
 
 
 public class Revision_General 
 {
  
  
- 
- 
- 
-    public void Revisar()
-    {
+    
+     public Servicio Revisar2(double monto)
+     {
+        String nombre_servicio="Revision General";
         
-        Menú revisar= new Menú();
-        
-        revisar.getContador();
-        revisar.getA_pagar();
-       revisar.getCadena();
-      
-        Servicio datos[]=new Servicio [1];
-       
-        
-        Inicializar_Precios revisar2=new Inicializar_Precios();
-        revisar2.setRevisionGeneral(0);
+        Servicio objeto_servicio=new Servicio(nombre_servicio,monto);
+         JOptionPane.showMessageDialog(null,"Servicio contratado: "+nombre_servicio+"\n"
+                 + "monto del servicio: "+monto);
+         return objeto_servicio;
+     }
+         
      
-       
-      
-       
-      
-           for(int j=revisar.getContador();j<revisar.getContador()+1;j++)
-           {
-               String nombre="Revision General";
-                int Gastos=(int)(revisar.getA_pagar()+revisar2.getRevisionGeneral());
-               
-               datos[j]=new Servicio(nombre, Gastos);
-               
-            revisar.setCadena((revisar.getCadena()+"\n"+"servicio: "+datos[j].getNombre()+"\n "+"Gastó: "+datos[j].getGastos()+" "));
-         
-        }
-           
-         
-       JOptionPane.showMessageDialog(null,revisar.getCadena());
-           
-           
-       
-       
-        revisar.setA_pagar((int) (revisar.getA_pagar()+revisar2.getRevisionGeneral()));
-        revisar.setContador(revisar.getContador()+1);
-       
-       
-    }
-    
-    
     
     
 }
