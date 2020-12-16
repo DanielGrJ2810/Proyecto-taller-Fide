25a0f13874036cadfba5d65184ab23a321884530
DanielGrJ2810 <73206195+DanielGrJ2810@users.noreply.github.com>
2020-12-15 20:10:27 -0600

    Revision_General

--- src/proyecto_taller_fide/Revision_General.java
+++ src/proyecto_taller_fide/Revision_General.java
@@ -1,5 +1,9 @@
 /*
-
+U.Fidelitas
+Introduccion a la Programacion
+Realizado por: Daniel Granados, Bryan Brenes, Jostin Bermudez.
+Programa Taller_FIDE
+17/12/2020
  */
 package proyecto_taller_fide;
 
